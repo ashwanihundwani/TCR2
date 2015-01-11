54cb2d5b0aec38f7514907a544a48da35ad22502
Vikram <vikkysingh@gmail.com>
2015-01-11 12:24:37 +0530

    test

--- ViewControllers/TinnitusCoachSoundViewController/TinnitusCoachSoundViewController.h
+++ ViewControllers/TinnitusCoachSoundViewController/TinnitusCoachSoundViewController.h
@@ -2,8 +2,7 @@
 //  TinnitusCoachSoundViewController.h
 //  TinnitusCoach
 //
-//  Created by Vikram Singh on 3/29/15.
-//  Copyright (c) 2015 Creospan. All rights reserved.
+ //  Copyright (c) 2015 Creospan. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
