7e9b97b8a34ce8cc81aa1e926a717fa88227cf08
Amit <amit@SakisMacbook.local>
2015-09-07 01:54:18 +0530

    Fix for ID 64

--- ViewControllers/NavigationTipsViewController/NavigationTipsViewController.m
+++ ViewControllers/NavigationTipsViewController/NavigationTipsViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
+    [Utils addSwipeGestureToView:self.view target:self selector:@selector(understoodButtonClicked:)];
+
     
 }
 
